1f2884b7c9bf43d466e9402627070939ecfccc6c
Damian <vdberg.damian@gmail.com>
2020-04-11 10:07:01 +0200

    Fix drawables not tinting pre API 21

--- app/src/main/java/com/dvdb/checklist/util/DrawableExtensions.kt
+++ app/src/main/java/com/dvdb/checklist/util/DrawableExtensions.kt
@@ -1,9 +1,14 @@
 package com.dvdb.checklist.util
 
 import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.annotation.ColorInt
 import androidx.core.graphics.drawable.DrawableCompat
 
 internal fun Drawable.setTintCompat(@ColorInt color: Int) {
-    DrawableCompat.setTint(this, color)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        DrawableCompat.setTint(this, color)
+    } else {
+        DrawableCompat.setTint(DrawableCompat.wrap(this), color)
+    }
 }
