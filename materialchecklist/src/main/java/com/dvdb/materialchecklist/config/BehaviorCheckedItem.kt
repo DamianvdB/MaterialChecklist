4552856e9ae9711d3be2031afd57c7b73fb5a5a4
Damian <vdberg.damian@gmail.com>
2020-04-18 16:20:18 +0200

    Add parsing helper methods for item behaviors.
    Add parsing methods for item behaviors for parsing int or string representations of the enum instances.

--- materialchecklist/src/main/java/com/dvdb/materialchecklist/config/BehaviorCheckedItem.kt
+++ materialchecklist/src/main/java/com/dvdb/materialchecklist/config/BehaviorCheckedItem.kt
@@ -4,5 +4,15 @@ enum class BehaviorCheckedItem {
     MOVE_TO_TOP_OF_CHECKED_ITEMS,
     MOVE_TO_BOTTOM_OF_CHECKED_ITEMS,
     KEEP_POSITION,
-    DELETE
+    DELETE;
+
+    companion object {
+        fun fromInt(value: Int): BehaviorCheckedItem {
+            return values().firstOrNull { it.ordinal == value } ?: MOVE_TO_TOP_OF_CHECKED_ITEMS
+        }
+
+        fun fromString(value: String): BehaviorCheckedItem {
+            return values().firstOrNull { it.name.equals(value, true) } ?: MOVE_TO_TOP_OF_CHECKED_ITEMS
+        }
+    }
 }
