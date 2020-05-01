97e3813439a08d1b53eb360e916a0d9a4fcb937e
Damian <vdberg.damian@gmail.com>
2020-05-01 14:35:38 +0200

    Add delegate to manage the focus of checklist items.

---
+++ materialchecklist/src/main/java/com/dvdb/materialchecklist/manager/ChecklistFocusManager.kt
@@ -0,0 +1,69 @@
+/*
+ * Designed and developed by Damian van den Berg.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dvdb.materialchecklist.manager
+
+import com.dvdb.materialchecklist.recycler.item.base.BaseRecyclerItem
+import com.dvdb.materialchecklist.recycler.item.checklist.ChecklistRecyclerItem
+
+/**
+ * Manages the focus and the selection behavior of checklist items.
+ */
+internal interface ChecklistFocusManager {
+
+    fun onItemFocusChanged(
+        position: Int,
+        startSelection: Int,
+        hasFocus: Boolean
+    )
+
+    fun onItemSelectionChanged(
+        position: Int,
+        startSelection: Int,
+        hasFocus: Boolean
+    )
+
+    fun onItemsSet(
+        originalItems: List<BaseRecyclerItem>,
+        updatedItems: List<BaseRecyclerItem>
+    )
+
+    fun onNewItemCreated(position: Int)
+
+    fun onItemPreCheckedStateChanged(position: Int)
+
+    fun onItemChecked(
+        originalPosition: Int,
+        updatedPosition: Int,
+        item: ChecklistRecyclerItem
+    )
+
+    fun onItemUnchecked(
+        originalPosition: Int,
+        updatedPosition: Int,
+        item: ChecklistRecyclerItem
+    )
+
+    fun onItemDeleted(
+        position: Int,
+        item: ChecklistRecyclerItem,
+        isDeletedIconClicked: Boolean
+    )
+
+    fun onAllCheckedItemsRemoved(itemPositions: List<Int>)
+
+    fun onItemDragStarted()
+}
